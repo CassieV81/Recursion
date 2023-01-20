5c63fd6aa0124a7e89f1c8eea9d51f4b538098c3
CassieV <obicassievirtue@gmail.com>
2023-01-20 22:24:26 +0100

    Starting afresh

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,37 +1,17 @@
 
-arr = [3, 2, 5, 9, 0, 7, 1, 2, 6, 2, 0, 3, 1]
-ex = [2, 0, 3, 1]
+arr = [3, 2, 5, 9, 0, 7, 1, 2, 6]
+ex = [2, 0]
 
 def merge_sort(n)
-  copy = n.dup
-  middle = (copy.length / 2).ceil
-  left = copy.slice(0, middle)
-  right = copy.slice(middle, copy.length)
-  sorted_array = []
-  merged_array = []
-  if left.length == 1 || right.length == 1
-    if left[0] < right[0]
-      p merged_array.push(left[0])
-      p merged_array.push(right[0])
-    elsif left[0] > right[0]
-      p merged_array.push(right[0])
-      p merged_array.push(left[0])
-    end
-  elsif left.length > 1 && right.length > 1
-    if copy.length > 2
-      left = merge_sort(left)
-      right = merge_sort(right)
-      until left.empty? || right.empty?
-        if left.first < right.first
-          merged_array.push(left.shift)
-        else
-          merged_array.push(right.shift)
-        end
-      end
-      sorted_array = merged_array + left + right
-    end
-    p sorted_array
+  if n.length <= 1
+    return n
   end
+  middle = n.length / 2
+  left = n[0...middle]
+  right = n[middle..-1]
+  p left
+  p right
+
 end
 
-merge_sort(ex)
+merge_sort(arr)
