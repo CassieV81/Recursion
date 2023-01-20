2b318e7ea431105c30dee12245d5ea0169eb615e
CassieV <obicassievirtue@gmail.com>
2023-01-20 20:53:43 +0100

    Resolved

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,5 +1,5 @@
 
-arr = [3, 2, 5, 9, 0, 7, 1, 2]
+arr = [3, 2, 5, 9, 0, 7, 1, 2, 6, 2, 0, 3, 1]
 ex = [2, 0, 3, 1]
 
 def merge_sort(n)
@@ -7,30 +7,31 @@ def merge_sort(n)
   middle = (copy.length / 2).ceil
   left = copy.slice(0, middle)
   right = copy.slice(middle, copy.length)
-  if left.length == 1 && right.length == 1
-    merged_array = []
+  sorted_array = []
+  merged_array = []
+  if left.length == 1 || right.length == 1
     if left[0] < right[0]
-      merged_array.push(left[0])
-      merged_array.push(right[0])
+      p merged_array.push(left[0])
+      p merged_array.push(right[0])
     elsif left[0] > right[0]
-      merged_array.push(right[0])
-      merged_array.push(left[0])
+      p merged_array.push(right[0])
+      p merged_array.push(left[0])
     end
   elsif left.length > 1 && right.length > 1
     if copy.length > 2
-      p left = merge_sort(left)
-      p right = merge_sort(right)
-      sorted_array = []
+      left = merge_sort(left)
+      right = merge_sort(right)
       until left.empty? || right.empty?
         if left.first < right.first
-          sorted_array.push(left.shift)
+          merged_array.push(left.shift)
         else
-          sorted_array.push(right.shift)
+          merged_array.push(right.shift)
         end
       end
-      p sorted_array + left + right
+      sorted_array = merged_array + left + right
     end
+    p sorted_array
   end
 end
 
-merge_sort(arr)
+merge_sort(ex)
